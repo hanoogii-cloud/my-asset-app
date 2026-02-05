49ef2dc27aeaaf4f078bb10310f96d9a4858956a
hanoogii-cloud <hanoogii@gmail.com>
2026-02-05 22:16:28 +0900

    Update assetapp.py

--- assetapp.py
+++ assetapp.py
@@ -24,8 +24,8 @@ DEFAULT_ASSETS = [
     {"symbol": "BTC", "count": 0.23},
     {"symbol": "ETH", "count": 3.0},
     {"symbol": "TSLA", "count": 40},
-    {"symbol": "GOOGL", "count": 25},
-    {"symbol": "PLTR", "count": 25},
+    {"symbol": "GOOGL", "count": 27},
+    {"symbol": "PLTR", "count": 20},
     {"symbol": "BMNR", "count": 100}
 ]
 
@@ -121,6 +121,7 @@ st.caption(f"마지막 업데이트: {datetime.now().strftime('%H:%M:%S')} (30�
 
 time.sleep(30)
 st.rerun()
+
 
 
 
