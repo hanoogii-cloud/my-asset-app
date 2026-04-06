cd259c4bf6e7c0b243ecf3fdc1cb8e7cff81eba0
hanoogii-cloud <hanoogii@gmail.com>
2026-04-07 00:47:57 +0900

    Update assetapp.py

--- assetapp.py
+++ assetapp.py
@@ -25,7 +25,7 @@ DEFAULT_ASSETS = [
     {"symbol": "BTC", "count": 0.22},
     {"symbol": "ETH", "count": 3},
     {"symbol": "TSLA", "count": 50},
-    {"symbol": "GOOGL", "count": 29},
+    {"symbol": "GOOGL", "count": 30},
     {"symbol": "PLTR", "count": 0}
 ]
 
