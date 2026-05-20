3c7e66ad71bbec89ba5acc8e4897f2c7a9620dbb
hanoogii-cloud <hanoogii@gmail.com>
2026-05-20 17:51:03 +0900

    Update assetapp.py

--- assetapp.py
+++ assetapp.py
@@ -32,7 +32,7 @@ krx_symbols = get_krx_names()
 DEFAULT_ASSETS = [
     {"symbol": "BTC", "count": 0.24},
     {"symbol": "ETH", "count": 3},
-    {"symbol": "TSLA", "count": 43},
+    {"symbol": "TSLA", "count": 42},
     {"symbol": "GOOGL", "count": 25},
     {"symbol": "PLTR", "count": 10},
     {"symbol": "0183J0", "count": 400} 
